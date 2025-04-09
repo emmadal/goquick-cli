43461b9c16b58e26b3c00222c7196c3d1ababc26
emmadal <emmanueldalougou@gmail.com>
2025-04-09 17:41:09 +0000

    chore: initialize cli tool config

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+const (
+	BLACK_BOLD = "\033[30;1m"
+	RESET      = "\033[0m"
+)
+
+type model struct {
+	command string
+}
+
+// Init handles the initial state of the program.
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+// Update handles the logic of the program.
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", ":q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+// View handles the display of the program.
+func (m model) View() string {
+	sb := strings.Builder{}
+
+	// Pre-allocate buffer with approximate size to avoid reallocations
+	sb.Grow(50)
+
+	sb.WriteString(`
+██████╗ ██╗   ██╗██╗ ██████╗██╗  ██╗
+██╔═══██╗██║   ██║██║██╔═══   ██║ ██╔╝
+██║   ██║██║   ██║██║██║      █████╔╝
+██║▄▄ ██║██║   ██║██║██║      ██╔═██╗
+╚██████╔╝╚██████╔╝██║╚██████╔ ██║  ██╗
+╚══▀▀═╝  ╚═════╝ ╚═╝ ╚═════╝ ╚═╝  ╚═╝
+	`)
+	sb.WriteString("\n")
+	sb.WriteString("Create fast and reliable projects with quick framework\n")
+	sb.WriteString("\n")
+	sb.WriteString(BLACK_BOLD + "USAGE:\n" + RESET)
+	sb.WriteString("$ quick <command>\n")
+	sb.WriteString("\n")
+	sb.WriteString(BLACK_BOLD + "COMMANDS:\n" + RESET)
+	sb.WriteString("q: \tquit\n")
+	sb.WriteString("init: \tcreate a new project\n")
+	sb.WriteString("addc: \tadd a new controller\n")
+	sb.WriteString("\n")
+	sb.WriteString(BLACK_BOLD + "EXAMPLES:\n" + RESET)
+	sb.WriteString("$ quick init\n")
+	sb.WriteString("$ quick addc controller_name\n")
+	return sb.String()
+}
+
+func main() {
+	if _, err := tea.NewProgram(model{}).Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running program: %s\n", err)
+		os.Exit(1)
+	}
+}
